0e39e6cabbf14aa4a3035bd2cd5c27eaafca9a99
i3-code <i3pobox@gmail.com>
2021-01-07 14:03:27 +0300

    fix: player sprite in matter world

--- rsclone/src/js/sprites/player.js
+++ rsclone/src/js/sprites/player.js
@@ -1,5 +1,14 @@
 import Phaser from 'phaser';
 
+function createPlayerAnimations(scene, key, sprite) {
+  scene.anims.create({
+    key: `move-${key}`,
+    frames: scene.game.anims.generateFrameNumbers(sprite),
+    frameRate: 31,
+    repeat: -1,
+  });
+}
+
 export default class Player extends Phaser.Physics.Matter.Sprite {
   constructor(scene, key, x, y, sprite) {
     const DEFAULT_MASS = 1; // number is random right now, can(should?) be changed later
@@ -11,18 +20,8 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
     };
     super(scene.matter.world, x, y, sprite);
     this.setFixedRotation(); // disable spin around its mass center point
-    /* setting equal mass to different size sprites, so
-    that characters will jump same height and run with the same speed */
     this.setMass(DEFAULT_MASS);
-    this.createPlayerAnimations(scene, key, sprite);
-  }
-
-  createPlayerAnimations(scene, key, sprite) {
-    scene.anims.create({
-      key: `move-${key}`,
-      frames: scene.game.anims.generateFrameNumbers(sprite),
-      frameRate: 31,
-      repeat: -1,
-    });
+    scene.add.existing(this);
+    createPlayerAnimations(scene, key, sprite);
   }
 }
