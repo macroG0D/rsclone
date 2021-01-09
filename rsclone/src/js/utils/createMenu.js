e5e526e5434bcad5e030adc5a6b7159923c0f096
i3-code <i3pobox@gmail.com>
2021-01-09 09:07:43 +0300

    refactor: strange declarations in createMenu.js

--- rsclone/src/js/utils/createMenu.js
+++ rsclone/src/js/utils/createMenu.js
@@ -21,16 +21,15 @@ const MENU_ITEM_HEIGHT = 60;
 export function createMenu(scene, menuItems, back = false, backCallback = () => scene.scene.start('MainMenu')) {
   const menuX = scene.cameras.main.centerX;
   const menuY = scene.cameras.main.centerY - MENU_ITEM_HEIGHT;
-  scene.cursors = scene.input.keyboard.createCursorKeys();
-  scene.esc = scene.input.keyboard.addKey('ESC');
+  scene.input.keyboard.createCursorKeys();
 
   Object.entries(menuItems).forEach(([itemName, itemLink], itemIndex) => {
     const menuItem = scene.add.text(
-        menuX,
-        menuY + (itemIndex * MENU_ITEM_HEIGHT),
-        itemName,
-        menuItemStyle,
-      )
+      menuX,
+      menuY + (itemIndex * MENU_ITEM_HEIGHT),
+      itemName,
+      menuItemStyle,
+    )
       .setOrigin(0.5)
       .setInteractive({
         useHandCursor: true,
@@ -49,7 +48,8 @@ export function createMenu(scene, menuItems, back = false, backCallback = () => 
       .on('pointerover', () => menuItemBack.setStyle(menuItemOverStyle))
       .on('pointerout', () => menuItemBack.setStyle(menuItemBackStyle))
       .on('pointerdown', backCallback);
-    scene.esc.on('down', backCallback);
+
+    scene.input.keyboard.addKey('ESC').on('down', backCallback);
   }
 }
 
