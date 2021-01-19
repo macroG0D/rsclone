8c9a90fc5e4a014cf4f7a4f7b435fe3ff8704ef8
i3-code <i3pobox@gmail.com>
2021-01-19 21:43:38 +0300

    feat: menu localisation

--- rsclone/src/js/utils/createMenu.js
+++ rsclone/src/js/utils/createMenu.js
@@ -1,13 +1,17 @@
 import Create from '../components/dom-create';
 
+import { LOCALE } from '../locale';
+
 export function createMenu(scene, menuItems, back = false, backCallback = () => scene.scene.switch('MainMenu')) {
   const menuX = scene.cameras.main.centerX;
   const menuY = scene.cameras.main.centerY;
+  const locale = LOCALE[scene.game.app.settings.locale];
   // scene.input.keyboard.createCursorKeys();
   const menu = new Create('div', false, 'game-menu');
   menu.items = {};
   Object.entries(menuItems).forEach(([itemName, itemLink], itemIndex) => {
-    const item = new Create('div', menu.node, 'game-menu-item', itemName);
+    const localItemName = locale[itemName];
+    const item = new Create('div', menu.node, 'game-menu-item', localItemName);
     if (!itemIndex) item.node.classList.add('game-menu-item-active');
     item.link = itemLink;
     item.index = itemIndex;
@@ -16,7 +20,8 @@ export function createMenu(scene, menuItems, back = false, backCallback = () => 
   });
 
   if (back) {
-    menu.back = new Create('div', menu.node, 'game-menu-item', 'back');
+    const localBackName = locale.back;
+    menu.back = new Create('div', menu.node, 'game-menu-item', localBackName);
     menu.back.node.classList.add('game-menu-item-back');
     menu.back.node.addEventListener('click', backCallback, false);
   }
