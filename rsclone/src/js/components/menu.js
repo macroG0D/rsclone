5ab5be93ca9f13341d770c472e17c5f138ca5914
i3-code <i3pobox@gmail.com>
2021-01-20 13:25:20 +0300

    feature: change menu's active item by keyboard input

--- rsclone/src/js/components/menu.js
+++ rsclone/src/js/components/menu.js
@@ -5,9 +5,10 @@ import { LOCALE } from '../locale';
 export default class Menu extends Create {
   constructor(scene, menuItems, back = false, backCallback = () => scene.scene.switch('MainMenu')) {
     super('div', false, 'game-menu');
+    this.scene = scene;
+    scene.input.keyboard.createCursorKeys();
     const { centerX, centerY } = scene.cameras.main;
     const locale = LOCALE[scene.game.app.settings.locale];
-    scene.input.keyboard.createCursorKeys();
     this.items = {};
 
     Object.entries(menuItems).forEach(([itemName, itemLink], itemIndex) => {
@@ -20,8 +21,11 @@ export default class Menu extends Create {
       item.link = itemLink;
       item.index = itemIndex;
       item.node.addEventListener('click', itemLink, false);
-      this.items[itemName] = item;
+      this.items[itemIndex] = item;
     });
+    scene.input.keyboard.addKey('UP').on('down', () => this.switchItem('up'));
+    scene.input.keyboard.addKey('DOWN').on('down', () => this.switchItem('down'));
+    scene.input.keyboard.addKey('ENTER').on('down', () => this.selectItem());
 
     if (back) {
       const localBackName = locale.back;
@@ -33,5 +37,28 @@ export default class Menu extends Create {
 
     this.spawn = scene.add.dom(centerX, centerY, this.node);
     this.spawn.setOrigin(0.5);
+  }
+
+  highlightItem(next) {
+    const current = this.activeItem;
+    if (next !== current) {
+      this.items[current].node.classList.remove('game-menu-item-active');
+      this.items[next].node.classList.add('game-menu-item-active');
+      this.activeItem = next;
+    }
+  }
+
+  switchItem(direction) {
+    const total = Object.keys(this.items).length - 1;
+    const current = this.activeItem;
+    const change = (direction === 'up') ? -1 : 1;
+    let next = current + change;
+    if (next < 0 || next > total) next = current;
+    if (next !== current) this.highlightItem(next);
+  }
+
+  selectItem(item = this.activeItem) {
+    const { link } = this.items[item];
+    link.call(this.scene);
   }
 }
