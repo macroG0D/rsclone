e34c1e397b85965c993d6fe94279338fe15a96aa
i3-code <i3pobox@gmail.com>
2021-01-22 21:47:36 +0300

    refactor: change scene after preload

--- rsclone/src/js/scenes/preload.js
+++ rsclone/src/js/scenes/preload.js
@@ -132,9 +132,8 @@ export default class Preload extends Phaser.Scene {
   }
 
   create() {
-    // this.scene.start('MainMenu');
+    this.scene.start('MainMenu');
     // this.scene.start('Level1');
-    this.scene.start('MainMenuSettings');
   }
 
   showBootBg() {
