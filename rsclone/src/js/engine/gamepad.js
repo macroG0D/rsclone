cd0253d39b6659dc55b0d6a0f148a4207faa9f73
i3-code <i3pobox@gmail.com>
2021-01-30 10:27:42 +0300

    refactor: keyboard input

--- rsclone/src/js/engine/gamepad.js
+++ rsclone/src/js/engine/gamepad.js
@@ -7,40 +7,33 @@ export default class Gamepad extends Phaser.Events.EventEmitter {
     super();
     this.scene = scene;
     this.playerKey = playerKey;
-    this.state = {};
     this.createGamepad(position, config);
-    /*
-    this.controls = {};
-    controls.forEach((controlKey, controlIndex) => {
-      const direction = CONTROL_KEYS_SEQUENCE[controlIndex];
-      this.controls[direction] = this.scene
-        .input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes[controlKey], true, true);
+  }
 
-      this.controls[direction].on('down', () => {
-        if (!this.scene.online) {
-          this.scene.setDirection(playerKey, direction, true);
-          return;
-        }
-        this.scene.client.sendData('playerMove', {
-          playerKey,
-          direction,
-          movementFlag: true,
-        });
-      });
+  createGamepad(position, config = '4dir') {
+    const { scene } = this;
+    const radius = 100;
+    const padding = 50;
+    const size = radius + padding;
+    const x = (position === 1) ? size : scene.game.config.width - size;
+    const y = scene.game.config.height - size;
 
-      this.controls[direction].on('up', () => {
-        if (!this.scene.online) {
-          this.scene.setDirection(playerKey, direction, false);
-          return;
-        }
-        this.scene.client.sendData('playerMove', {
-          playerKey,
-          direction,
-          movementFlag: false,
-        });
-      });
+    const mainColor = (position === 1) ? 0x23a645 : 0xde2e6b;
+    const secondaryColor = (position === 1) ? 0xde2e6b : 0x23a645;
+
+    const joystick = scene.plugins.get('rexVirtualJoystick').add(scene, {
+      x,
+      y,
+      radius,
+      base: scene.add.circle(0, 0, radius, mainColor),
+      thumb: scene.add.circle(0, 0, radius / 2, secondaryColor),
+      dir: config,
     });
-    */
+    joystick.base.alpha = 0.75;
+    joystick.base.setDepth(1);
+    joystick.thumb.setDepth(2);
+    joystick.on('update', this.joystickOnUpdate, this);
+    this.joystick = joystick;
   }
 
   joystickOnUpdate() {
@@ -89,31 +82,5 @@ export default class Gamepad extends Phaser.Events.EventEmitter {
       direction,
       movementFlag: false,
     });
-  }
-
-  createGamepad(position, config = '4dir') {
-    const { scene } = this;
-    const radius = 100;
-    const padding = 50;
-    const size = radius + padding;
-    const x = (position === 1) ? size : scene.game.config.width - size;
-    const y = scene.game.config.height - size;
-
-    const mainColor = (position === 1) ? 0x23a645 : 0xde2e6b;
-    const secondaryColor = (position === 1) ? 0xde2e6b : 0x23a645;
-
-    const joystick = scene.plugins.get('rexVirtualJoystick').add(scene, {
-      x,
-      y,
-      radius,
-      base: scene.add.circle(0, 0, radius, mainColor),
-      thumb: scene.add.circle(0, 0, radius / 2, secondaryColor),
-      dir: config,
-    });
-    joystick.base.alpha = 0.75;
-    joystick.base.setDepth(1);
-    joystick.thumb.setDepth(2);
-    joystick.on('update', this.joystickOnUpdate, this);
-    this.joystick = joystick;
   }
 }
