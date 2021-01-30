afea54d1d07c80e8d17b01b5a7accba796e8827b
TonYem <tonyrhesus@gmail.com>
2021-01-31 00:25:01 +0200

    feat: english version of website

--- rsclone/src/js/localehtml.js
+++ rsclone/src/js/localehtml.js
@@ -4,36 +4,36 @@ export const LOCALE_HTML = {
     home: 'home',
     about: 'about',
     // стартовый текст
-    start: 'Клон 2D игры-головоломки, в которую можно играть как в одиночку, так и в паре за одной клавиатурой, или по сети. Финальный проект курса front-end курса <b>RSS</b>',
+    start: 'A JavaScript clone of 2D puzzle game that can be played solo or co-op with a friend at the same keyboard, or even online. Final project of front-end course of <b>Rolling Scopes School</b>',
     // кнопка играть
-    play: 'play',
+    play: 'play now',
     // промо текст
-    promo: 'В ходе прохождения игры игроки столкнутся с живописным миром, взаимодействием двух персонажей, с приятной физикой, включающей головоломки, врагов и даже эксперименты с гравитацией.',
+    promo: 'During the game walkthrough, players will encounter a picturesque world, interaction between two cute characters, enjoyable physics, puzzles, enemies, and even experiments with a gravity.',
 
     gameplayTitle: 'Gameplay',
-    gameplayText: 'Игровой процесс построен на взаимодействии двух главных героев — Ibb и Obb, которые помогая друг другу преодолевать препятствия должны как можно быстрее добраться до конца игры и при этом заработать как можно больше очков.',
+    gameplayText: 'The gameplay is built around the interaction of the two main characters — Ibb and Obb, which help each other to overcome obstacles to get the end of the game as quickly as possible while earning as many points as they can.',
 
     ratingTitle: 'Rating table',
-    ratingText: 'Никнеймы игроков с лучшими результатами прохождения игры попадут в глобальную рейтинговую таблицу.',
+    ratingText: 'The names of the players with the best in-game results will get a record in the global ranking table.',
 
     scoreTitle: 'Final score',
-    scoreText: 'Игроки набирают очки убивая врагов и собирая кристаллики, которые из них выпадают. Но в глобальную рейтинговую таблицу попадают лишь те, кто наберет максимальное количество очков и за минимальное время прохождения игры.',
+    scoreText: 'Players score points by killing enemies and collecting crystals that fall out of their corpses. But only those who earn the biggest score within shortest game time will make it to the global ranking table.',
 
-    enemyTitle: 'Враги и боевая система',
-    enemyText: 'Каждый враг в игре обладает двумя сущностями — «лицевой» и «тыльной» лицевая сторона чем-то напоминает ежа, столкнувшись с которой персонажи погибают, и игра завершается. А тыльная сторона, является слабой точкой врагов и при столкновении персонажей с ней, враг погибает, разбрасывая ценные кристаллы.',
+    enemyTitle: 'Enemies and Combat System',
+    enemyText: 'Each enemy in the game has two entities - a "front" and a "back". The front side looks like something like a hedgehog, when one of the characters collide with it, he dies and the game is over. The hedgehog\'s back side, on the other hand, is the weak point of those enemies, and when the characters collide with it, the enemy dies, scattering valuable crystals.',
 
-    multyTitle: 'Мультиплеер',
-    multyText: 'Пройти игру можно в одиночку, играя сразу за обоих игроков со своей клавиатуры. Можно проходить игру с напарником, также с одной клавиатуры. Но можно и создать онлайн сессию, к которой сможет подключиться любой желающий по сети для совместного онлайн прохождения.',
+    multyTitle: 'Multiplayer',
+    multyText: 'You can complete the game alone, playing for both characters at the same time or to pass the game with a partner at the same keyboard. It is possible even to create an online session that any user can join to for a joint online walkthrough.',
 
     control: 'Control',
     jump: 'jump',
     left: 'left',
     right: 'right',
 
-    slogan: 'Сыграй в нашу версию ibb & obb',
+    slogan: 'Try our ibb & obb version',
 
     aboutTitle: 'About',
-    aboutText: '',
+    aboutText: 'The final project of the Rolling Scopes School frontend course completed in the beginning of 2021. This is a team assignment whose goal is to develop a JavaScript game that runs in a web browser. The choice of the game itself is the team discretion, and we decided to choose the game "Ibb & Obb"',
 
     team: 'Team',
     tony: 'Tony Yemelyanov',
@@ -41,7 +41,7 @@ export const LOCALE_HTML = {
     heliken: 'Filip Bursh',
     mauta: 'Anna Opareva',
 
-    copyright: 'Авторские права на большую часть материалов и саму концепцию принадлежат создателям оригинальной игры Ibb & Obb (издатель: Sparpweed, разработчики: Sparpweed, Codeglue)',
+    copyright: 'This is just a small study project, the copyright for most of the content and the game concept itself belongs to the creators of the original Ibb & Obb game (publisher: Sparpweed, developers: Sparpweed, Codeglue)',
 
   },
 
@@ -50,7 +50,7 @@ export const LOCALE_HTML = {
     home: 'главная',
     about: 'о проекте',
     // стартовый текст
-    start: 'Клон 2D игры-головоломки, в которую можно играть как в одиночку, так и в паре за одной клавиатурой, или по сети. Финальный проект курса front-end курса <b>RSS</b>',
+    start: 'JavaScript клон 2D игры-головоломки, в которую можно играть как в одиночку, так и в паре за одной клавиатурой, или по сети. Финальный проект front-end курса <b>Rolling Scopes School</b>',
     // кнопка играть
     play: 'играть',
     // промо текст
@@ -79,7 +79,7 @@ export const LOCALE_HTML = {
     slogan: 'Сыграй в нашу версию ibb & obb',
 
     aboutTitle: 'О проекте',
-    aboutText: 'Финальный проект фронтэнд курса RSSchool выполненный в начале 2021. Это командное задание, целью которого является разработка игры, работающей в веб браузере. Выбор самой игры был на усмотрение команды, и мы решили выбрать игру “Ibb & Obb”',
+    aboutText: 'Финальный проект фронтэнд курса RSSchool выполненный в начале 2021. Это командное задание, целью которого является разработка JavaScript игры, работающей в веб браузере. Выбор самой игры был на усмотрение команды, и мы решили выбрать игру “Ibb & Obb”',
 
     team: 'Team',
     tony: 'Тони Емельянов',
@@ -87,7 +87,7 @@ export const LOCALE_HTML = {
     heliken: 'Филипп Бурш',
     mauta: 'Анна Опарева',
 
-    copyright: 'Авторские права на большую часть материалов и саму концепцию принадлежат создателям оригинальной игры Ibb & Obb (издатель: Sparpweed, разработчики: Sparpweed, Codeglue)',
+    copyright: 'Это учебный проект. Авторские права на большую часть использованных материалов и на саму концепцию принадлежат создателям оригинальной игры Ibb & Obb (издатель: Sparpweed, разработчики: Sparpweed, Codeglue)',
   },
 };
 
