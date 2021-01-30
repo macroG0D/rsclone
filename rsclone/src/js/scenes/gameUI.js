03c8a6635b68f59fa12451523c634b0fbf0915d6
i3-code <i3pobox@gmail.com>
2021-01-30 13:20:19 +0300

    refactor: player controls now depends on client os type

--- rsclone/src/js/scenes/gameUI.js
+++ rsclone/src/js/scenes/gameUI.js
@@ -40,17 +40,24 @@ export default class gameUI extends Phaser.Scene {
     this.score = new Score(this, score, time);
     this.game.score = this.score;
 
+    const { desktop } = this.sys.game.device.os;
     if (online) {
       const playerKey = (master) ? 'ibb' : 'obb';
       parent.online = true;
       parent.playerKey = playerKey;
-      this.player1Input = new Input(parent, playerKey, PLAYER_1_CONTROLS);
+      if (desktop) {
+        this.player1Input = new Input(parent, playerKey, PLAYER_1_CONTROLS);
+      } else {
+        this.player1Input = new Gamepad(this, parent, playerKey, 1, 'left&right');
+        this.player2Input = new Gamepad(this, parent, playerKey, 2, 'up&down');
+      }
       parent.network.initSync();
-    } else {
+    } else if (desktop) {
       this.player1Input = new Input(parent, 'ibb', PLAYER_1_CONTROLS);
       this.player2Input = new Input(parent, 'obb', PLAYER_2_CONTROLS);
-      this.player1Gamepad = new Gamepad(this, parent, 'ibb', 1);
-      this.player2Gamepad = new Gamepad(this, parent, 'obb', 2);
+    } else {
+      this.player1Input = new Gamepad(this, parent, 'ibb', 1);
+      this.player2Input = new Gamepad(this, parent, 'obb', 2);
     }
   }
 
