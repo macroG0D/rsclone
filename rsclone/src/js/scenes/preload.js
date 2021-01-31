944e1000d6ac12cc47fd838989775626b472cd73
i3-code <i3pobox@gmail.com>
2021-01-31 06:27:16 +0300

    refactor: codestyle

--- rsclone/src/js/scenes/preload.js
+++ rsclone/src/js/scenes/preload.js
@@ -134,8 +134,8 @@ export default class Preload extends Phaser.Scene {
   }
 
   create() {
-    // this.scene.start('MainMenu');
-    this.scene.start('Level1');
+    this.scene.start('MainMenu');
+    // this.scene.start('Level1');
   }
 
   showBootBg() {
