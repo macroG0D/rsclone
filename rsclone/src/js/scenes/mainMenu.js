5daa87966384e58cc9f16145f77844a793f04f89
i3-code <i3pobox@gmail.com>
2021-02-02 12:07:05 +0300

    refactor: remove continue menu

--- rsclone/src/js/scenes/mainMenu.js
+++ rsclone/src/js/scenes/mainMenu.js
@@ -9,25 +9,14 @@ export default class MainMenu extends Phaser.Scene {
   }
 
   create() {
-    const { level, time, score } = this.game.app.settings;
-    let menuItems = {
+    const menuItems = {
       newGame: () => this.scene.start('MainMenuPlay'),
       leaderboard: () => this.scene.start('MainMenuLeaderBoard'),
       settings: () => this.scene.start('MainMenuSettings'),
       developers: () => this.scene.start('MainMenuDevelopers'),
       about: () => window.open('https://github.com/macroG0D/rsclone'),
     };
-    /* if (level > 1) {
-      menuItems = {
-        continue: () => this.continueGame({ level, time, score }), ...menuItems,
-      };
-    }  */
     this.menu = new Menu(this, menuItems);
-  }
-
-  continueGame(data) {
-    const { level } = data;
-    this.scene.start(`Level${level}`, data);
   }
 
   update() {
